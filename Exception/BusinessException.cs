e34c54a8eec4d8a371bc9ca2a0ab7395ac66466b
Long <longs3515@gmail.com>
2026-06-30 14:24:30 +0700

    use fast endpoint

--- Exception/BusinessException.cs
+++
@@ -1,8 +0,0 @@
-using bank.Entity;
-
-namespace bank.Exceptions;
-
-public class BusinessException(List<ApiError> errors) : Exception
-{
-    public List<ApiError> Errors { get; } = errors;
-}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,8 @@
 using FastEndpoints;
-using FastEndpoints.Swagger;
-using FluentValidation;
-using bank.Entity;
-using Microsoft.EntityFrameworkCore;
-using bank.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddValidatorsFromAssemblyContaining<Program>(ServiceLifetime.Transient);
-builder.Services.AddFastEndpoints();
-builder.Services.SwaggerDocument();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
-app.UseGlobalExceptionHandler();
 
-app.UseFastEndpoints(c =>
-{
-    c.Errors.ResponseBuilder = (failures, statusCode, _) =>
-    {
-        var errors = failures.Select(f => new ApiError(f.PropertyName, f.ErrorMessage)).ToList();
-        return ApiResponse<object>.ErrorResponse(
-            400, "Validation failed", errors);
-    };
-});
-
-app.UseSwaggerGen();
-
+builder.Services.AddFastEndpoints();
+app.UseFastEndpoints();
 app.Run();
